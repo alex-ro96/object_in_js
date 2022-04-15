0216d58e79d1fdf9456a2f360cc1d8c363284aca
Alexandra <alexandra.chamero-rossy@hotmail.com>
2022-04-15 11:02:51 +0200

    Exercise business card

--- object.js
+++ object.js
@@ -3,3 +3,15 @@ var city = {name: "Nyon", country: "Switzerland",
 
 console.log(city.country);
 console.log(city.name);
+
+//business card exercise
+
+var card = {firstName: "Harry",
+           lastName: "Potter",
+           phone: 78118299,
+           city: "London",
+           email: "harry.potter@poudlard.com"} ; 
+
+console.log(card.firstName);
+console.log(card.city);
+console.log(card["email"]); 
